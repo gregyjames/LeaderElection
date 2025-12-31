24f6a27131167eef998b956223756c91d239fff0
Greg James <gregvjames99@gmail.com>
2025-12-30 18:10:36 -0600

    Add FusionCache-based leader election implementation
    Introduced the LeaderElection.FusionCache project with an initial FusionCacheLeaderElection class implementing ILeaderElection. Updated the solution file to include the new project. The implementation is currently a stub with methods not yet implemented.

---
+++ LeaderElection.FusionCache/FusionCacheLeaderElection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LeaderElection.FusionCache;
+
+public class FusionCacheLeaderElection(ILogger<FusionCacheLeaderElection> logger, ZiggyCreatures.Caching.Fusion.FusionCache fusionCache): ILeaderElection
+{
+    public async ValueTask DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsLeader { get; }
+    public DateTime LastLeadershipRenewal { get; }
+    public event EventHandler<bool>? LeadershipChanged;
+    public event EventHandler<Exception>? ErrorOccurred;
+    public async Task StartAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> TryAcquireLeadershipAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RunTaskIfLeaderAsync(Func<Task> task, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RunTaskIfLeaderAsync(Action task, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
