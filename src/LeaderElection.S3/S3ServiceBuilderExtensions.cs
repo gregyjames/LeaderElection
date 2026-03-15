5e15f556d1fd4a88f41cfeb0b155723ddd9ae088
Greg James <gregvjames99@gmail.com>
2026-03-14 21:51:58 -0500

    feat: Add S3 Leader Election Implementation
    feat: Add HostBuilder extensions

---
+++ src/LeaderElection.S3/S3ServiceBuilderExtensions.cs
@@ -0,0 +1,40 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeaderElection.S3;
+
+public static class S3ServiceBuilderExtensions
+{
+    public static IServiceCollection AddS3LeaderElection(
+        this IServiceCollection services, 
+        Action<S3Settings> configureOptions)
+    {
+        services.Configure(configureOptions);
+        services.AddSingleton<S3LeaderElection>();
+        services.AddSingleton<ILeaderElection>(sp => sp.GetRequiredService<S3LeaderElection>());
+        
+        return services;
+    }
+
+    public static IServiceCollection AddS3LeaderElection(
+        this IServiceCollection services,
+        S3Settings options)
+    {
+        services.Configure<S3Settings>(opt =>
+        {
+            opt.InstanceId = options.InstanceId;
+            opt.BucketName = options.BucketName;
+            opt.LeaseDuration = options.LeaseDuration;
+            opt.RetryInterval = options.RetryInterval;
+            opt.RenewInterval = options.RenewInterval;
+            opt.ObjectKey = options.ObjectKey;
+            opt.EnableGracefulShutdown = options.EnableGracefulShutdown;
+            opt.MaxRetryAttempts = options.MaxRetryAttempts;
+        });
+
+        services.AddSingleton<S3Settings>();
+        services.AddSingleton<S3LeaderElection>();
+        services.AddSingleton<ILeaderElection>(sp => sp.GetRequiredService<S3LeaderElection>());
+        
+        return services;
+    }
+}
