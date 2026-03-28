8b2c76c4286c9bfe2b61ab5bc3c8c5f75fe36b0b
Greg James <gregvjames99@gmail.com>
2026-03-27 23:56:51 -0500

    refactor(Example): Enable warnings as errors

--- examples/LeaderElectionTester/Settings.cs
+++ examples/LeaderElectionTester/Settings.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using LeaderElection.Redis;
 
 namespace LeaderElectionTester;
 
+#pragma warning disable CA1812
 internal sealed class Settings
 {
     public RedisSettings Redis { get; init; } = new();
