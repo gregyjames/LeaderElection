c9736227ec8596bce81ae6526b6d57740c51a128
John Belcher <4042025+mrfootoyou@users.noreply.github.com>
2026-03-31 03:41:01 -0400

    feat(fusioncache): Extend DI and factory configuration options

--- src/LeaderElection.FusionCache/FusionCacheSettings.cs
+++ src/LeaderElection.FusionCache/FusionCacheSettings.cs
@@ -1,9 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using ZiggyCreatures.Caching.Fusion;
 
 namespace LeaderElection.FusionCache;
 
 public class FusionCacheSettings : LeaderElectionSettingsBase
 {
+    /// <summary>
+    /// Factory function to create an instance of IFusionCache. This allows for
+    /// custom cache implementations or retrieval logic. If not set, the default
+    /// IFusionCache registered in the service provider will be used.
+    /// </summary>
+    public Func<FusionCacheSettings, IFusionCache>? CacheFactory { get; set; }
+
     /// <summary>
     /// The key used to acquire the lock in the cache.
     /// This should be unique to avoid conflicts with other locks in the cache.
@@ -25,4 +33,17 @@ public class FusionCacheSettings : LeaderElectionSettingsBase
         nameof(FusionCacheSettingsValidator.ValidateLockExpiry)
     )]
     public TimeSpan LockExpiry { get; set; } = TimeSpan.FromSeconds(30);
+
+    /// <summary>
+    /// Copies the FusionCache settings from the source to the destination.
+    /// </summary>
+    public static void Copy(FusionCacheSettings src, FusionCacheSettings dst)
+    {
+        ArgumentNullException.ThrowIfNull(src);
+        ArgumentNullException.ThrowIfNull(dst);
+        LeaderElectionSettingsBase.Copy(src, dst);
+        dst.LockKey = src.LockKey;
+        dst.LockExpiry = src.LockExpiry;
+        dst.CacheFactory = src.CacheFactory;
+    }
 }
