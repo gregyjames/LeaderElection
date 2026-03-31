54b6f4541a9629b3a6ce51e17b7d0a1bfa6ebd12
John Belcher <4042025+mrfootoyou@users.noreply.github.com>
2026-03-31 03:41:01 -0400

    feat(distributedcache): Enhance DI and factory extensibility

--- src/LeaderElection.DistributedCache/DistributedCacheSettings.cs
+++ src/LeaderElection.DistributedCache/DistributedCacheSettings.cs
@@ -1,9 +1,17 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace LeaderElection.DistributedCache;
 
 public class DistributedCacheSettings : LeaderElectionSettingsBase
 {
+    /// <summary>
+    /// Factory function to create an instance of IDistributedCache. This allows for
+    /// custom cache implementations or retrieval logic. If not set, the default
+    /// IDistributedCache registered in the service provider will be used.
+    /// </summary>
+    public Func<DistributedCacheSettings, IDistributedCache>? CacheFactory { get; set; }
+
     /// <summary>
     /// The key used to acquire the lock in the distributed cache.
     /// This should be unique to avoid conflicts with other applications
@@ -26,4 +34,17 @@ public class DistributedCacheSettings : LeaderElectionSettingsBase
         nameof(DistributedCacheSettingsValidator.ValidateLockExpiry)
     )]
     public TimeSpan LockExpiry { get; set; } = TimeSpan.FromSeconds(30);
+
+    /// <summary>
+    /// Copies the DistributedCache settings from the source to the destination.
+    /// </summary>
+    public static void Copy(DistributedCacheSettings src, DistributedCacheSettings dst)
+    {
+        ArgumentNullException.ThrowIfNull(src);
+        ArgumentNullException.ThrowIfNull(dst);
+        LeaderElectionSettingsBase.Copy(src, dst);
+        dst.LockKey = src.LockKey;
+        dst.LockExpiry = src.LockExpiry;
+        dst.CacheFactory = src.CacheFactory;
+    }
 }
